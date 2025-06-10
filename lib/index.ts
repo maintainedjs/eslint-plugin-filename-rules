e01f6761384eef72f9d86ffa38713fe03187a3d7
Fatih Kaya <1994274@gmail.com>
2025-06-10 16:22:50 +0300

    expose `plugin`

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ import { ESLint } from "eslint";
 import { match } from "./match";
 import { notMatch } from "./notMatch";
 
-const plugin: ESLint.Plugin = {
+export const plugin: ESLint.Plugin = {
   rules: {
     match,
     "not-match": notMatch,
